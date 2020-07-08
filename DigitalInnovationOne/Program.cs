4770d629fac7e5a6577a19b9432dbb1fa3c6eb1f
Guilherme Soares Correa <55603259+guisoares1@users.noreply.github.com>
2020-07-07 23:25:20 -0300

    Add files via upload

---
+++ DigitalInnovationOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nNumVez=5;
+            while (nNumVez>=0){
+                Console.WriteLine($"Bem vindo ao curso de .NET {nNumVez}");
+                nNumVez--;
+            }
+        }
+    }
+}
